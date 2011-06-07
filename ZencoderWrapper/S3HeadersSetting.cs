5a3908a41f93f51db4ea7694e0d8fb06d5d44521
Craig Kitterman <craig@kitterman.net>
2011-06-07 16:03:38 -0700

    Added S3 HTTP Header support to output params

---
+++ ZencoderWrapper/S3HeadersSetting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ZencoderWrapper
+{
+    public class S3HeadersSetting
+    {
+        [JsonProperty(PropertyName = "Cache-Control")]
+        public string cache_control {get;set;}
+
+        [JsonProperty(PropertyName = "Content-Disposition")]
+        public string content_disposition { get; set; }
+
+        [JsonProperty(PropertyName = "Content-Encoding")]
+        public string content_encoding { get; set; }
+
+        [JsonProperty(PropertyName = "Content-Type")]
+        public string content_type { get; set; }
+
+        [JsonProperty(PropertyName = "Expires")]
+        public string expires { get; set; }
+
+        [JsonProperty(PropertyName = "x-amz-acl")]
+        public string x_amz_acl { get; set; }
+
+        [JsonProperty(PropertyName = "x-amz-storage-class")]
+        public string x_amz_storage_class { get; set; }
+
+    }
+}
